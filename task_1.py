c7d40bcf933b1a56ddcc9817e703961eb87fa705
Daria_Yakymova <kiritocun.ru@gmail.com>
2023-06-03 14:02:55 +0300

    Task 1 is done

--- task_1.py
+++ task_1.py
@@ -2,25 +2,26 @@
 # Необходимо вывести на экран названия дня недели.
 # Например, если 1, то на экране надпись понедельник, 2 — вторник и т.д.
 
-try:
-    user_day = int(input("Enter a number from 1 to 7: "))
-    if user_day == 1:
-        print(f"{user_day}st day of the week is Monday")
-    elif user_day == 2:
-        print(f"{user_day}nd day of the week is Tuesday")
-    elif user_day == 3:
-        print(f"{user_day}rd day of the week is Wednesday")
-    elif user_day == 4:
-        print(f"{user_day}th day of the week is Thursday ")
-    elif user_day == 5:
-        print(f"{user_day}th day of the week is Friday")
-    elif user_day == 6:
-        print(f"{user_day}th day of the week is Saturday")
-    elif user_day == 7:
-        print(f"{user_day}th day of the week is Sunday")
-    else:
-        raise Exception("Please enter number from 1 to 7")
-except ValueError as error:
-    print("Please, enter only int numbers")
-except Exception as error:
-    print("Please, enter int from 1 to 7")
+# try:
+#     user_day = int(input("Enter a number from 1 to 7: "))
+#     if user_day == 1:
+#         print(f"{user_day}st day of the week is Monday")
+#     elif user_day == 2:
+#         print(f"{user_day}nd day of the week is Tuesday")
+#     elif user_day == 3:
+#         print(f"{user_day}rd day of the week is Wednesday")
+#     elif user_day == 4:
+#         print(f"{user_day}th day of the week is Thursday ")
+#     elif user_day == 5:
+#         print(f"{user_day}th day of the week is Friday")
+#     elif user_day == 6:
+#         print(f"{user_day}th day of the week is Saturday")
+#     elif user_day == 7:
+#         print(f"{user_day}th day of the week is Sunday")
+#     else:
+#         raise Exception("Please enter number from 1 to 7")
+# except ValueError as error:
+#     print("Please, enter only int numbers")
+# except Exception as error:
+#     print(error)
+
